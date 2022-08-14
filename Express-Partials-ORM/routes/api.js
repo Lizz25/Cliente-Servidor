bb8bfe0a6fb1d606fcc67f5988ab34f2d7289219
Lizz25 <lizpevacacela@espol.edu.ec>
2022-08-14 06:11:24 -0500

    guias

---
+++ Express-Partials-ORM/routes/api.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+const Sequelize = require('sequelize')
+const Producto = require('../models').producto
+
+router.get('/productos', function(req, res, next) {
+    Producto.findAll({  
+         attributes: { exclude: ["updatedAt"] }  
+     })  
+    .then(resultado => {  
+        res.json(resultado)
+    }) 
+     .catch(error => res.status(400).send(error)) 
+ });
+
+router.get('/productos/:id', function(req, res, next) {
+    let id_req = req.params.id
+    Producto.findOne({
+        where:{id:id_req}
+    })
+    .then(resultado => {
+        res.json(resultado)
+    })
+    .catch(error => res.status(400).send(error))
+});
+ 
+ module.exports = router;
